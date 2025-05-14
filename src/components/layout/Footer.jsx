f2a7f3e01bf1e5f46df0c5bd9a6b9d11b1160b71
Mohammad El Prince <126906029+elprince-dev@users.noreply.github.com>
2025-05-14 01:35:08 -0400

    Updated Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -56,6 +56,7 @@ function Footer() {
       setSubscribing(false)
     }
   }
+  console.log(settings)
   
   return (
     <footer className="bg-gray-50 dark:bg-gray-900 pt-12 pb-6 border-t border-gray-200 dark:border-gray-800">
