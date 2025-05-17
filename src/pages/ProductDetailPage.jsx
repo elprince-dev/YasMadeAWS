389871fdd5167c5cf23eec0ba21c87fa66b71c55
Mohammad El Prince <126906029+elprince-dev@users.noreply.github.com>
2025-05-17 01:18:55 -0400

    Updated ProductDetailPage.jsx

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -70,9 +70,9 @@ function ProductDetailPage() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="container mx-auto px-4 py-8 mt-14 container-custom"
+      className="container mx-auto px-4 py-8 mt-14"
     >
-      <div className="grid md:grid-cols-2 gap-8">
+      <div className="grid md:grid-cols-2 gap-8 container-custom">
         <div className="relative">
           {product.image_url ? (
             <img
