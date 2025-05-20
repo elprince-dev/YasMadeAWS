50e75d3d1288d8ff35cd47f91ad73d324e5a7ec9
Mohammad El Prince <126906029+elprince-dev@users.noreply.github.com>
2025-05-19 23:02:12 -0400

    Implement E-transfer Payment System

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,5 @@ import OrderConfirmationPage from './pages/OrderConfirmationPage';
 
 // Add this route inside the Routes component
 <Route path="/order-confirmation/:id" element={<OrderConfirmationPage />} />
+
+export default OrderConfirmationPage
