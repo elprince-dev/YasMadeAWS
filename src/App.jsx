d0536613de2a889697d367963568c4833f52cf39
Mohammad El Prince <126906029+elprince-dev@users.noreply.github.com>
2025-05-19 23:03:45 -0400

    Fix Route component definition in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-// Update the routes in App.jsx to include the new OrderConfirmationPage
+import { Route, Routes } from 'react-router-dom';
 import OrderConfirmationPage from './pages/OrderConfirmationPage';
 
-// Add this route inside the Routes component
-<Route path="/order-confirmation/:id" element={<OrderConfirmationPage />} />
+function App() {
+  return (
+    <Routes>
+      <Route path="/order-confirmation/:id" element={<OrderConfirmationPage />} />
+    </Routes>
+  );
+}
 
-export default OrderConfirmationPage
+export default App;
